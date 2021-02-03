8d1e2e3de6604f674bc6823077f2613155511e82
grillzwitu <grillzwitu@gmail.com>
2021-02-03 15:21:15 +0000

    ubuntu_4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  * Description: the program starts and ends here.
  * Return: value 0 to exit
  */
-int main (void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
