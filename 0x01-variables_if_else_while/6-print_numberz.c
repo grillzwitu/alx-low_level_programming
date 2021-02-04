adaa91fe4721fd8b593919f948db567c800e9135
grillzwitu <grillzwitu@gmail.com>
2021-02-04 15:30:16 +0100

    update README and commit 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digits = 48;
+for (digits = 48; digits < 58; digits++)
+{
+putchar(digits);
+}
+putchar('\n');
+return (0);
+}
