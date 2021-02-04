9f60a60b4053c717e5165abac8d55cdc25f336bd
grillzwitu <grillzwitu@gmail.com>
2021-02-04 20:54:23 +0100

    update README and commit 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digits;
+for (digits = 0; digits < 100; digits++)
+{
+putchar((digits/10) + '0');
+putchar((digits%10) + '0');
+if (digits < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
