eb7f14ab424d25ccbe98eb4f9127018e804d4341
grillzwitu <grillzwitu@gmail.com>
2021-02-04 19:59:50 +0000

    commit_10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,8 +9,8 @@ int main(void)
 int digits;
 for (digits = 0; digits < 100; digits++)
 {
-putchar((digits/10) + '0');
-putchar((digits%10) + '0');
+putchar((digits / 10) + '0');
+putchar((digits % 10) + '0');
 if (digits < 99)
 {
 putchar(',');
