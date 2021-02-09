0f93257479615ba498cff690defebfae67e784c4
grillzwitu <grillzwitu@gmail.com>
2021-02-09 18:44:52 +0000

    corrected

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,22 +15,17 @@ int largest;
 if (a > b)
 {
 if (a > c)
-{
+
 largest = a;
-}
+
 else
-{
 largest = c;
 }
-}
+
 else if (b > c)
-{
 largest = b;
-}
 else
-{
 largest = c;
-}
 
 return (largest);
 }
