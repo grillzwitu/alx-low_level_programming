247e7e3a8cb094c29f5d6d1d1e0926f16bf4c579
grillzwitu <grillzwitu@gmail.com>
2021-02-09 18:52:58 +0000

    ubuntu_corrected

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,22 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b)
+if (a > b && a > c)
 {
-if (a > c)
-
 largest = a;
-
-else
+}
+else if (a > b && c > a)
+{
 largest = c;
 }
-
 else if (b > c)
+{
 largest = b;
+}
 else
+{
 largest = c;
+}
 
 return (largest);
 }
