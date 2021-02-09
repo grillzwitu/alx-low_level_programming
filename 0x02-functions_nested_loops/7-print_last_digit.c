2ca18170012477e41f81d01318a92cd688064d0f
grillzwitu <grillzwitu@gmail.com>
2021-02-09 01:50:20 +0100

    corrected

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,35 @@
  * Description: print_last_digit Prints the last digit of the argument.
  *
  * Return: 0 (Success).
+ *
+ * _abs - entry point
+ *
+ * @num: int argument
+ *
+ * Description: _abs Prints the absolute value of the argument.
+ *
+ * Return: 0 (Success).
  */
+
+int _abs(int num)
+{
+if (num > 0)
+{
+return (num);
+}
+else if (num < 0)
+{
+num = -num;
+return (num);
+}
+
+return (0);
+}
 
 int print_last_digit(int num)
 {
 int lastdig;
-if (num < 0)
-{
-num = -num;
-}
-lastdig = num % 10;
+lastdig = _abs((num % 10));
 _putchar(lastdig + '0');
 return (lastdig);
 }
