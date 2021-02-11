78ad7166cf4da3dc74a9add770b26836aea1a205
grillzwitu <grillzwitu@gmail.com>
2021-02-11 12:15:53 +0000

    corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,16 +20,16 @@ if (n > 0)
 {
 for (i = 1; i <= n; i++)
 {
-for (j = 0; j < i; j++)
+for (j = 1; j < i; j++)
 {
 _putchar(space);
 }
 _putchar(line);
 _putchar(10);
 }
+}
 else
 {
 _putchar(10);
-}
 }
 }
