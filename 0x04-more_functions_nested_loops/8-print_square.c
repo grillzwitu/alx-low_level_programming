f15275888ff298b5446ff23ea5063fe670bce2c1
grillzwitu <grillzwitu@gmail.com>
2021-02-11 12:25:59 +0000

    corrected

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,7 +4,7 @@
  * print_square - entry point
  *
  * @size: int argument for size of the square
- * 
+ *
  * Description: Prints a diagonal line in the terminal
  *
  * Return: Void
@@ -15,18 +15,20 @@ void print_square(int size)
 char frag;
 int i, j;
 frag = '#';
-for ( i = 1; i <= size; i++)
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
 {
 for (j = 1; j < size; j++)
 {
 _putchar(frag);
 }
-if (size < 0)
-{
-_putchar(10);
-}
 _putchar(frag);
 _putchar(10);
 }
-
+}
+else
+{
+_putchar(10);
+}
 }
