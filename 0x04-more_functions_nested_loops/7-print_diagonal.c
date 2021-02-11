6630fcbc01086d25ac85338f5e993f4db0877500
grillzwitu <grillzwitu@gmail.com>
2021-02-11 12:07:27 +0000

    corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,7 +4,7 @@
  * print_diagonal - entry point
  *
  * @n: int argument for number of lines
- * 
+ *
  * Description: Prints a diagonal line in the terminal
  *
  * Return: Void
@@ -16,18 +16,20 @@ char line, space;
 int i, j;
 line = '\\';
 space = ' ';
-for ( i = 1; i <= n; i++)
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
 {
 for (j = 0; j < i; j++)
 {
 _putchar(space);
 }
-if (n < 0)
-{
-_putchar(10);
-}
 _putchar(line);
 _putchar(10);
 }
-
+else
+{
+_putchar(10);
+}
+}
 }
