cf917bffdfdb61577e9a06e417e26d7f1fdbc6c3
grillzwitu <grillzwitu@gmail.com>
2021-02-17 11:13:56 +0100

    commit atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,17 +12,17 @@
 
 int _atoi(char *s)
 {
-while (*s != '\0')
-{
-int i;
-int dig = 0;
-int sign = -1;
-int brk = 0;
+int i, dig, sign, brk;
+dig = 0;
+sign = -1;
+brk = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == '-')
+{
 sign *= -1;
+}
 if (s[i] >= '0' && s[i] <= '9')
 {
 dig *= 10;
@@ -30,7 +30,9 @@ dig -= (s[i] - '0');
 brk = 1;
 }
 else if (brk == 1)
+{
 break;
+}
 }
 dig = sign * dig;
 return (dig);
