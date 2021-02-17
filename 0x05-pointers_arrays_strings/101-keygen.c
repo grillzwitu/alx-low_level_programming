ac2b8fe27f052d630e1c8299d181dd8fb8e6db54
grillzwitu <grillzwitu@gmail.com>
2021-02-17 13:09:05 +0100

    coomit 101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,26 +11,9 @@
 
 int main(void)
 {
-int count, sum, n;
-int pass[100];
-
-sum = 0;
-
-srand(time(NULL));
-
-for (count = 0; count < 100; count++)
-{
-pass[count] = rand() % 78;
-sum += (pass[count] + '0');
-putchar(pass[count] + '0');
-if ((2772 - sum) - '0' < 78)
-{
-n = 2772 - sum - '0';
-sum += n;
-putchar(n + '0');
-break;
-}
-}
-
+int num;
+srand(time(0));
+num = rand();
+printf("%i\n",num);
 return (0);
 }
