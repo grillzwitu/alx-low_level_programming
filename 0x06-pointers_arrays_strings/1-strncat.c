cf550713b02c300d9690b51029145dee6e3de847
grillzwitu <grillzwitu@gmail.com>
2021-02-18 16:55:53 +0000

    corrected

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,8 +16,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
-for (i=0; dest[i] != '\0'; ++i);
-for (j=0; src[j] != '\0' && j < n; ++j, ++i)
+for (i = 0; dest[i] != '\0'; ++i)
+;
+for (j = 0; src[j] != '\0' && j < n; ++j, ++i)
 {
 dest[i] = src[j];
 }
