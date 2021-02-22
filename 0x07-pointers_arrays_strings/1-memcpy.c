35ab1e822d5122d4ae5deb9f787cb0e5df715a43
grillzwitu <grillzwitu@gmail.com>
2021-02-22 12:45:19 +0000

    commit 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 # include "holberton.h"
 
 /**
- * *memcpy - entry point
+ * *_memcpy - entry point
  *
  * @dest: pointer argument
  * @src: char argument
@@ -16,56 +16,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-for ( i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 *dest++ = *src++;
 }
 return (dest);
-}
-
-#include <stdio.h>
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
 }
