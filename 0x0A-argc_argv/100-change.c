d0565e950e1eae0fd26e5b22730667316ffeef51
grillzwitu <grillzwitu@gmail.com>
2021-02-27 19:02:25 +0000

    commit 100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,42 +17,35 @@ int main(int argc, char *argv[])
 {
 int cents, coin_count;
 if (argc != 2)
-{
 printf("Error\n");
 return (1);
-}
+
 coin_count = 0;
 cents = atoi(argv[1]);
+
 if (cents < 0)
-{
 printf("0\n");
 return (0);
-}
+
 while (cents >= 25)
-{
 cents -= 25;
 coin_count++;
-}
+
 while (cents >= 10)
-{
 cents -= 10;
 coin_count++;
-}
+
 while (cents >= 5)
-{
 cents -= 5;
 coin_count++;
-}
+
 while (cents >= 2)
-{
 cents -= 2;
 coin_count++;
-}
+
 while (cents >= 1)
-{
 cents -= 1;
 coin_count++;
-}
 
 printf("%d\n", coin_count);
 return (0);
