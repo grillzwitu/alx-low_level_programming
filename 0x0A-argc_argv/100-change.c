da749202e929876182c56995193fd6d79a84f564
grillzwitu <grillzwitu@gmail.com>
2021-02-27 20:34:44 +0000

    commit 100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,57 +3,77 @@
 #include <stdlib.h>
 
 /**
+ * coin_count - entry point
+ *
+ * @cents: int argument
+ *
+ * Description: Counts the least number of coins to give as change
+ *
+ * Return: 0 (Success)
+ */
+
+int coin_count(int cents)
+{
+int count;
+count = 0;
+
+while (cents >= 25)
+{
+cents -= 25;
+count++;
+}
+while (cents >= 10)
+{
+cents -= 10;
+count++;
+}
+while (cents >= 5)
+{
+cents -= 5;
+count++;
+}
+while (cents >= 2)
+{
+cents -= 2;
+count++;
+}
+while (cents >= 1)
+{
+cents -= 1;
+count++;
+}
+
+printf("%d\n", count);
+return (0);
+}
+
+/**
  * main - entry point
  *
  * @argc: int argument(count)
  * @argv: char argument(vector)
  *
- * Description: Counts the number of coins to give as change
+ * Description: Implements coin count as required
  *
  * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-int cents, coin_count;
+int amount;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-coin_count = 0;
-cents = atoi(argv[1]);
-if (cents < 0)
+
+amount = atoi(argv[1]);
+if (amount < 0)
 {
 printf("0\n");
 return (0);
 }
-while (cents >= 25)
-{
-cents -= 25;
-coin_count++;
-}
-while (cents >= 10)
-{
-cents -= 10;
-coin_count++;
-}
-while (cents >= 5)
-{
-cents -= 5;
-coin_count++;
-}
-while (cents >= 2)
-{
-cents -= 2;
-coin_count++;
-}
-while (cents >= 1)
-{
-cents -= 1;
-coin_count++;
-}
 
-printf("%d\n", coin_count);
+coin_count(amount);
 return (0);
 }
