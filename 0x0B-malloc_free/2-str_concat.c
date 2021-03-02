613fca275f1bd210eb656e52885f780141267064
grillzwitu <grillzwitu@gmail.com>
2021-03-02 20:20:53 +0100

    corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+ * _strlen - entry point
+ *
+ * @s: char argument
+ *
+ * Description: Counts the lenght of the string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int i, l;
+l= 0;
+
+for (i = 0; s[i] != '\0'; i++)
+l += 1;
+
+return (l);
+}
+
+/**
  * *str_concat - entry point
  *
  * @s1: pointer argument
@@ -14,10 +35,7 @@
 char *str_concat(char *s1, char *s2)
 {
 char *str;
-int c_c, l, a;
-
-c_c = 0;
-l = 0;
+int l1, l2, a, b;
 
 if (!s1)
 s1 = "";
@@ -25,19 +43,21 @@ s1 = "";
 if (!s2)
 s2 = "";
 
-for (a = 0; s1[a] || s2[a]; a++)
-l++;
+l1 = _strlen(s1);
+l2 = _strlen(s2);
 
-str = malloc(sizeof(str) * l);
+str = malloc(sizeof(str) * (l1 + l2) +1);
 
 if (str == NULL)
 return (NULL);
 
-for (a = 0; a < s1[a]; a++)
-str[c_c++] = s1[a];
+for (a = 0; a < l1; a++)
+str[a] = s1[a];
 
-for (a = 0; a < s2[a]; a++)
-str[c_c++] = s2[a];
+for (b = 0; b < l2; b++, a++)
+str[a] = s2[b];
+
+str[a] = '\0';
 
 return (str);
 }
