5366e93758aa6f734fbd2db8cb5b378142e068fd
grillzwitu <grillzwitu@gmail.com>
2021-03-04 15:18:12 +0100

    array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+# include "holberton.h"
+
+/**
+ * *array_range - entry point
+ *
+ * @min: int argument
+ * @max: int argument
+ *
+ * Description: Creates an array of integers.
+ *
+ * Return: Pointer to new array.
+ */
+
+int *array_range(int min, int max)
+{
+int *arr;
+int c, range;
+
+if (min > max)
+{
+return (NULL);
+}
+
+range = max - min;
+
+arr = malloc(sizeof(int) * (range + 1));
+
+if (arr == NULL)
+return (NULL);
+
+for (c = 0; c < range; c++)
+arr[c] = c + min;
+
+arr[range] = max;
+
+return(arr);
+}
