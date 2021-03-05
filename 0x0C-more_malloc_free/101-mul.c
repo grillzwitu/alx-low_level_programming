2cc3f4d64687faae07a37c0dbad9dad6bad71054
grillzwitu <grillzwitu@gmail.com>
2021-03-05 21:05:33 +0100

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,114 @@
 # include "holberton.h"
+# include <string.h>
 
 /**
- * main - entry point
- *
- * @argc: int argument
- * @argv: int argument
- *
- * Description: Multiplies two positive numbers.
- *
- * Return: 0.
+ * _isdigit - checks for a digit
+ * @c: Digit
+ * Return: 1 or 0 
  */
 
-int main(int argc, char *argv[])
+int _isdigit(int c)
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+return (c >= '0' && c <= '9');
+}
 
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (s[len])
+len++;
+
+return (len);
+}
+
+/**
+ * mult - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+char *mult(char *s1, char *s2)
+{
+char *r;
+int l1, l2, a, b, c, x;
+
+l1 = _strlen(s1);
+l2 = _strlen(s2);
+r = malloc(a = x = l1 + l2);
+if (!r)
+printf("Error\n"), exit(98);
+while (a--)
+r[a] = 0;
+
+for (l1--; l1 >= 0; l1--)
+{
+if (!_isdigit(s1[l1]))
+{
+free(r);
+printf("Error\n"), exit(98);
+}
+a = s1[l1] - '0';
+c = 0;
+
+for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+{
+if (!_isdigit(s2[l2]))
+{
+free(r);
+printf("Error\n"), exit(98);
+}
+b = s2[l2] - '0';
+
+c += r[l1 + l2 + 1] + (a *b);
+r[l1 + l2 + 1] = c % 10;
+
+c /= 10;
+}
+if (c)
+r[l1 + l2 + 1] += c;
+}
+return (r);
+}
+
+
+/**
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
+{
+char *r;
+int a, c, x;
+
+if (argc != 3)
+printf("Error\n"), exit(98);
+
+x = _strlen(argv[1]) + _strlen(argv[2]);
+r = mult(argv[1], argv[2]);
+c = 0;
+a = 0;
+while (c < x)
+{
+if (r[c])
+a = 1;
+if (a)
+putchar(r[c] + '0');
+c++;
+}
+if (!a)
+putchar('0');
+putchar('\n');
+free(r);
 return (0);
 }
