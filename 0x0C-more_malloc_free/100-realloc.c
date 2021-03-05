29454ae1f5f8b29ac5f128572ecabf109faa927b
grillzwitu <grillzwitu@gmail.com>
2021-03-05 08:21:59 +0100

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,107 @@
+# include "holberton.h"
+
+/**
+ * *_realloc - entry point
+ *
+ * @ptr: pointer argument
+ * @old_size: int argument
+ * @new_size: int argument
+ *
+ * Description: Reallocates a memory block using malloc and free.
+ *
+ * Return: Pointer to reallocated memory block.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *temp, *reAlloc;
+unsigned int c;
+
+if (ptr != NULL)
+{
+temp = ptr;
+}
+else
+{
+return (malloc(new_size));
+}
+
+if (new_size == old_size)
+{
+return (ptr);
+}
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (0);
+}
+
+reAlloc = malloc(new_size);
+
+if (reAlloc == NULL)
+{
+return (0);
+}
+
+for (c = 0; c < (old_size || c < new_size); c++)
+{
+reAlloc[c] = temp[c];
+}
+
+free(ptr);
+
+return (reAlloc);
+}
+
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *p;
+    int i;
+
+    p = malloc(sizeof(char) * 10);
+    p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+    i = 0;
+    while (i < 98)
+    {
+        p[i++] = 98;
+    }
+    simple_print_buffer(p, 98);
+    free(p);
+    return (0);
+}
