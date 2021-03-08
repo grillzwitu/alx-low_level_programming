a4aaa66869d38c0732d5ed6e354fc727101eb8dd
grillzwitu <grillzwitu@gmail.com>
2021-03-08 14:26:25 +0100

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * *new_dog - entry point
@@ -16,30 +17,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
-int l1, l2, a;
+int a, b, c;
 
-l1 = l2 = a = 0;
-while (name[l1++] != '\0')
-;
-while (owner[l2++] != '\0')
-;
+a = 0, b = 0;
+
+while (name[a] != '\0')
+a++;
+while (owner[b] != '\0')
+b++;
 
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
+{
+free (new_dog);
 return (NULL);
+}
 
-new_dog->name = malloc(l1);
-new_dog->owner = malloc(l2);
+new_dog->name = malloc(a * sizeof(new_dog->name));
+if (new_dog->name == NULL)
+{
+free (new_dog->name);
+free (new_dog);
+return (NULL);
+}
 
-do {
-new_dog->name[a] = name[a];
-} while (name[a++] != '\0');
-a = 0;
+for (c = 0; c <= a; c++)
+new_dog->name[c] = name[c];
 
-do {
-new_dog->owner[a] = owner[a];
-} while (owner[a++] != '\0');
 new_dog->age = age;
+
+new_dog->owner = malloc(b * sizeof(new_dog->owner));
+if (new_dog->owner == NULL)
+{
+free (new_dog->owner);
+free (new_dog->name);
+free (new_dog);
+return (NULL);
+}
+
+for (c = 0; c <= a; c++)
+new_dog->owner[c] = owner[c];
 
 return (new_dog);
 }
