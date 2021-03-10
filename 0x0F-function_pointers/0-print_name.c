2dfb8e34c582960a0d1833b96dff0ce768b6fbfc
grillzwitu <grillzwitu@gmail.com>
2021-03-10 19:36:21 +0100

    print-name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+# include "function_pointers.h"
+
+/**
+ * print_name - entry point
+ *
+ * @name: pointer argument for name to be printed
+ * @f: pointer function argument
+ *
+ * Description: Prints name 
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+## 0x0F-function_pointers
+
+- 0-print_name.c:
