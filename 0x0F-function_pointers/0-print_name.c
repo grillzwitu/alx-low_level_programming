dd712760e735a862e542cac1b10d310fd57bb6c6
grillzwitu <grillzwitu@gmail.com>
2021-03-10 19:39:56 +0100

    print-name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,11 @@
  * @name: pointer argument for name to be printed
  * @f: pointer function argument
  *
- * Description: Prints name 
+ * Description: Prints name
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name)
 f(name);
 }
