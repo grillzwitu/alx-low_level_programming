301127f2aac680719ecbbd15153e69c5fad06264
grillzwitu <grillzwitu@gmail.com>
2021-03-18 14:45:16 +0000

    first-line

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first_out - Entry Point
+ *
+ * Description: Prints before the main function
+ *
+ * Return: Void
+ */
+
+void first_out(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
