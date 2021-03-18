bf75e0a75fe877bd905904c89e10c389315a08cf
grillzwitu <grillzwitu@gmail.com>
2021-03-18 13:56:38 +0000

    free-list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - Entry Point
+ *
+ * @head: pointer argument (points to the head of the linked list)
+ *
+ * Description: Frees list_t
+ *
+ * Return: Void
+ */
+
+void free_list(list_t *head)
+{
+        list_t *temp;
+	for (; head != NULL;)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
