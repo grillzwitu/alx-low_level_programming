947bc72649cf84d9c28d000dca870c4be52f2a5a
grillzwitu <grillzwitu@gmail.com>
2021-03-18 11:48:37 +0000

    list-len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len - Entry Point
+ *
+ * @h: list argument
+ *
+ * Description: Counts the lenght of a singly linked list
+ *
+ * Return: Number of elements in the linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nod_idx;
+
+	for (no_idx = 0; h != NULL; nod_idx++)
+	{
+		if (h == NULL)
+		{
+			return (nod_idx);
+		}
+		h = h->next;
+	}
+
+	return (nod_idx);
+}
