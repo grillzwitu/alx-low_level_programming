3710b88c546a32f80d48ac8758699126fabe406a
grillzwitu <grillzwitu@gmail.com>
2021-03-29 18:30:27 +0000

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,17 +21,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	text = malloc(sizeof(char) * letters + 1);
-
 	if (text == NULL)
 		return (0);
-
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 	{
 		free(text);
 		return (0);
 	}
-
 	no_ltrs = read(file, text, sizeof(char) * letters);
 	if (no_ltrs == -1)
 	{
@@ -39,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file);
 		return (0);
 	}
-
 	no_ltrs = write(STDOUT_FILENO, text, no_ltrs);
 	if (no_ltrs == -1)
 	{
