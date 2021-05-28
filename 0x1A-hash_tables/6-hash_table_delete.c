089ea58b4c01bf07fbbb047a95383e3864348156
grillzwitu <grillzwitu@gmail.com>
2021-05-28 11:11:41 +0000

    hash_table_delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,23 +12,23 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-        unsigned long int i;
-        hash_node_t *next;
+	unsigned long int i;
+	hash_node_t *next;
 
-        if (ht == NULL)
-                return;
+	if (ht == NULL)
+		return;
 
-        for (i = 0; i < ht->size; i++)
-        {
-                while (ht->array[i] != NULL)
-                {
-                        next = ht->array[i];
-                        free(next->key);
-                        free(next->value);
-                        ht->array[i] = ht->array[i]->next;
-                        free(next);
-                }
-        }
-        free(ht->array);
-        free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			next = ht->array[i];
+			free(next->key);
+			free(next->value);
+			ht->array[i] = ht->array[i]->next;
+			free(next);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
