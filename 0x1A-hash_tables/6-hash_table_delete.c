4bef94fa1d2faf62623aba3bc0cd9f66d8c9487a
Tare Appah <grillzwitu@gmail.com>
2021-05-28 12:07:15 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,19 +12,23 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *ptr, *next;
-unsigned long int idx;
+        unsigned long int i;
+        hash_node_t *next;
 
-if (ht == NULL)
-return;
-for (idx = 0; idx < ht->size; idx++)
-for (ptr = ht->array[idx]; ptr != NULL; ptr = next)
-{
-next = ptr->next;
-free(ptr->key);
-free(ptr->value);
-free(ptr);
-}
-free(ht->array);
-free(ht);
+        if (ht == NULL)
+                return;
+
+        for (i = 0; i < ht->size; i++)
+        {
+                while (ht->array[i] != NULL)
+                {
+                        next = ht->array[i];
+                        free(next->key);
+                        free(next->value);
+                        ht->array[i] = ht->array[i]->next;
+                        free(next);
+                }
+        }
+        free(ht->array);
+        free(ht);
 }
