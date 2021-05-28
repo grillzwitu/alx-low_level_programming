3b340dff172e8b8491eb87b6ef5c5a9708c0d64a
grillzwitu <grillzwitu@gmail.com>
2021-05-28 10:17:32 +0000

    get corrected

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -23,20 +23,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	position = key_index((unsigned char *)key, ht->size);
 	if (ht->array[position] == NULL)
-	{
 		return (NULL);
-	}
 	if (strcmp(ht->array[position]->key, key) == 0)
-	{
 		return (ht->array[position]->value);
-	}
 	if (ht->array[position]->next != NULL)
-	{
 		for (tmp = ht->array[position]->next; tmp != NULL; tmp = tmp->next)
 		{
 			if (strcmp(tmp->key, key) == 0)
 				return (tmp->value);
 		}
 	return (NULL);
-	}
 }
